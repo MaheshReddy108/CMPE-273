12fa983830d3fa3c3d0f17d16a22c7ea488011f8
MaheshReddy108 <maheshreddy.ms18@gmail.com>
2019-10-05 23:36:02 -0700

    updated search, cuisine and orders

--- Lab 1 - 013823095/Grubhub/Backened/index.js
+++ Lab 1 - 013823095/Grubhub/Backened/index.js
@@ -59,7 +59,7 @@ const upload = multer({ storage });
 app.post('/login', function (req, res) {
 
     console.log('Inside login POST');
-    console.log('Request Body: ', req.body);
+    console.log('1.Request Body: ', req.body);
 
     //Query
 
@@ -74,7 +74,7 @@ app.post('/login', function (req, res) {
         else {
 
             //Login validation query
-            var sql = 'SELECT * from BUYER WHERE Email = ' + mysql.escape(req.body.email) +  'UNION SELECT * from BUYER WHERE Email = '+ mysql.escape(req.body.email);
+            var sql = 'SELECT Fname,Lname,Email,Password from BUYER WHERE Email = ' + mysql.escape(req.body.email) +  'UNION SELECT Fname,Lname,Email,Password from OWNER WHERE Email = '+ mysql.escape(req.body.email);
             conn.query(sql, function (err, result) {
 
                 if (err) {
@@ -103,17 +103,14 @@ app.post('/login', function (req, res) {
                             httpOnly: false,
                             path: '/'
                         });
-                        res.cookie('Email', result[0].Email, {
-                            maxAge: 360000,
-                            httpOnly: false,
-                            path: '/'
-                        });
+                        console.log("2.result[0]: ", result[0]);
                         req.session.user = result[0];
+                        console.log("3.req.session.user: ",req.session.user);
                         res.writeHead(200, {
                             'Content-type': 'text/plain'
                         })
                         console.log('Login successful!');
-                        res.end('Login successful!');
+                        res.end('4.Login successful!');
                     }
 
                 }
@@ -216,7 +213,7 @@ app.post('/Owner-signup', function (req, res) {
 
     //User creation query
 
-    var presql = 'SELECT Buyer_id from Owner where Email = ' + mysql.escape(req.body.Email);
+    var presql = 'SELECT Owner_id from Owner where Email = ' + mysql.escape(req.body.Email);
 
     pool.getConnection(function (err, conn) {
 
@@ -256,12 +253,14 @@ app.post('/Owner-signup', function (req, res) {
                             const hashedPassword = hash;
                             console.log(hashedPassword);
                             
-                            var sql = 'INSERT into BUYER (Fname, Lname,Res_name,Email,Res_address,Res_zip,Cuisine,Locations,Password) VALUES(' +
+                            var sql = 'INSERT into OWNER (Fname, Lname,Res_name,Email,Res_address,Res_zip,Phone,Cuisine,Locations,Password) VALUES(' +
                             mysql.escape(req.body.FirstName) + ',' +
                             mysql.escape(req.body.LastName) + ',' +
+                            mysql.escape(req.body.RestaurantName) + ',' +
                             mysql.escape(req.body.Email) + ',' +
-                            mysql.escape(req.body.RestaurantAdress) + ',' +
+                            mysql.escape(req.body.RestaurantAddress) + ',' +
                             mysql.escape(req.body.RestaurantZipcode) + ',' +
+                            mysql.escape(req.body.RestaurantPhoneNumber) + ',' +
                             mysql.escape(req.body.Cuisine) + ',' +
                             mysql.escape(req.body.Locations) + ',' +
                              mysql.escape(hashedPassword )+ ');';
@@ -303,9 +302,11 @@ app.post('/Owner-signup', function (req, res) {
 //Buyer Profile Details
 app.get('/Buyer-profile-details', function (req, res) {
 
-    console.log('Inside Buyer Profile Details GET!');
-    console.log('Request Body:', req.body);
+    console.log('5.Inside Buyer Profile Details GET!');
+    console.log('6.Request Body:', req.body);
 
+ console.log(`7. last ${req.session.user}`);
+ 
     if (req.session.user) {
 
         pool.getConnection(function (err, conn) {
@@ -411,14 +412,15 @@ app.post('/Buyer-update-profile', function (req, res) {
 
             }
             else {
-
+                   console.log("this is id: ",req.session.user.Buyer_id);
+                   
                 var sql = 'UPDATE BUYER set ' +
-                    'Fname = ' + mysql.escape(req.body.Firstname) + ',' +
-                    'Lname = ' + mysql.escape(req.body.Lastname) + ',' +
-                    'Email = ' + mysql.escape(req.body.userEmail) + ',' +
-                    'Phone = ' + mysql.escape(req.body.userPhone) + ',' +
-                    'Address = ' + mysql.escape(req.body.userAdr) + ',' +
-                    'ProfileImage = ' + mysql.escape(req.body.ProfileImage) +
+                    'Fname = ' + mysql.escape(req.body.FirstName) + ',' +
+                    'Lname = ' + mysql.escape(req.body.LastName) + ',' +
+                    'Email = ' + mysql.escape(req.body.UserEmail) + ',' +
+                    'Phone = ' + mysql.escape(req.body.UserPhone) + ',' +
+                    'Address = ' + mysql.escape(req.body.UserAdr) + 
+                
                     ' WHERE Buyer_id = ' + req.session.user.Buyer_id;
 
                 conn.query(sql, function (err, result) {
@@ -527,11 +529,6 @@ app.post('/download-file/:file(*)', (req, res) => {
 
 
 
-
-
-
-
-
 //Logout
 
 app.post('/logout', function (req, res) {
@@ -544,6 +541,105 @@ app.post('/logout', function (req, res) {
     res.end('Back to login!');
 
 });
+
+//Search
+app.post('/search', function (req, res) {
+
+    console.log('Inside Search Method GET!');
+    console.log('Request Body: ', req.body);
+
+    const searchProperties = req.body;
+
+    if (req.session.user) {
+
+        pool.getConnection(function (err, conn) {
+            if (err) {
+                console.log('Error in creating connection!');
+                res.writeHead(400, {
+                    'Content-type': 'text/plain'
+                });
+                res.end('Error in creating connection!');
+            }
+            else {
+
+                //Search Properties Query            
+                var sql = 'SELECT * from OWNER Join ITEMS ON OWNER.Owner_id=ITEMS.Owner_id and OWNER.Res_name=ITEMS.Res_name  WHERE ' +
+                    ' ITEMS.Item_name like % ' + mysql.escape(searchProperties.searchText) +'%' +
+                    ' AND OWNER.Cuisine like %' + mysql.escape(searchProperties.cuisine)+'%';
+
+                conn.query(sql, function (err, result) {
+
+                    if (err) {
+                        console.log('Error in Retrieving property data');
+                        res.writeHead(400, {
+                            'Content-type': 'text/plain'
+                        });
+                        res.end('Error in Retrieving property data');
+
+                    }
+                    else {
+                        console.log(JSON.stringify(result));
+                        res.writeHead(200, {
+                            'Content-type': 'application/json'
+                        });
+                        res.end(JSON.stringify(result));
+                    }
+                });
+            }
+        });
+    }
+});
+
+
+//Get Property Details
+
+app.post('/property-details', function (req, res) {
+
+    console.log('Inside Restaurant Details Method POST!');
+    console.log('Request Body: ', req.body);
+
+    if (req.session.user) {
+
+        pool.getConnection(function (err, conn) {
+            if (err) {
+
+                console.log('Error in creating connection!');
+                res.writeHead(400, {
+                    'Content-type': 'text/plain'
+                });
+                res.end('Error in creating connection!');
+
+            }
+            else {
+
+                var sql = 'SELECT * from OWNER Join ITEMS ON OWNER.Owner_id=ITEMS.Owner_id and OWNER.Res_name=ITEMS.Res_name WHERE Res_name = ' + req.body.Res_name;
+                conn.query(sql, function (err, result) {
+                    if (err) {
+                        console.log('Error in Retrieving restaurant');
+                        res.writeHead(400, {
+                            'Content-type': 'text/plain'
+                        });
+                        res.end('Error in Retrieving Restaurant');
+
+                    }
+                    else {
+                        res.writeHead(200, {
+                            'Content-type': 'application/json'
+                        });
+                        console.log(JSON.stringify(result[0]));
+                        res.end(JSON.stringify(result[0]));
+                        conn.release();
+                    }
+                });
+            }
+
+        });
+    }
+
+});
+
+
+
 
 
 
