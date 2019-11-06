488282c4d815e74a613d1c7172ba1ce056e5fe1d
MaheshReddy108 <maheshreddy.konatham@sjsu.edu>
2019-11-06 04:57:47 -0800

    connection pooling

--- Lab 2 - 013823095/Backend/ConnectionPooling.js
+++ Lab 2 - 013823095/Backend/ConnectionPooling.js
@@ -1,11 +1,28 @@
-var mysql = require('mysql');
+var express = require('express');  
+var app = express();  
+var MongoClient = require('mongodb').MongoClient;  
+var assert = require('assert');
 
-var pool = mysql.createPool({
-    connectionLimit: 100,
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'homeaway'
+// Connection URL
+var url = 'mongodb+srv://mahesh:Mahesh1896@273-nu1yk.mongodb.net/test?retryWrites=true&w=majority';
+
+// start server on port 3000
+app.listen(3000, '0.0.0.0', function() {  
+  // print a message when the server starts listening
+  console.log("server starting");
 });
 
-module.exports = pool;
+// Use connect method to connect to the server when the page is requested
+app.get('/', function(request, response) {  
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.listCollections({}).toArray(function(err, collections) {
+        assert.equal(null, err);
+        collections.forEach(function(collection) {
+            console.log(collection);
+        });
+        db.close();
+    })
+    response.send('Connected - see console for a list of available collections');
+  });
+});
